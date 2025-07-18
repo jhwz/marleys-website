94f9ac5fdfe1c2a5715f2690b9e12f7bc95648aa
Joel Howse <joel@helicalco.com>
2025-07-18 20:22:52 +1200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+	import type { LayoutProps } from './$types';
+
+	let { children }: LayoutProps = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+
+
+<h1>Welcome to Marley's Website</h1>
